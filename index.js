cf2551bb7bed88929cc358055854c13a3cf23c8e
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-03-25 00:59:00 +0300

    first class function lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+// Code your solution is in this file!
+const drivers = ['Antonia','Nuru','Amari','Mo'];
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0, 2);
+}
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(2, 4);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (fare){
+    return function fareQuintupler(fare){
+        return fare * 5;
+    };
+}
+const fareDoubler = function(fare){
+    return fare * 2;
+}
+const fareTripler = function(fare){
+    return fare * 3;
+}
+const selectDifferentDrivers = function (drivers, funct){
+    return funct(drivers)
+}
